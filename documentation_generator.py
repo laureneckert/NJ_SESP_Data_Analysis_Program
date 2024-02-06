73e49c3b272bd114546ac4c24dbb6736fe95af64
Lauren Eckert <laureneckert5@gmail.com>
2024-02-06 14:46:41 -0500

    working on documentation generator

--- documentation_generator.py
+++ documentation_generator.py
@@ -1,12 +1,8 @@
-#NJSESP
-#Lauren Eckert
-
 import subprocess
 import os
 import shutil
-import re
 
-def generate_uml_diagrams(package_path, output_directory, project_name='MyProject'):
+def generate_uml_diagrams(package_path, output_directory, project_name='NJSESP_Dot_Rough_Draft'):
     # Ensure output directory exists
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
@@ -15,7 +11,7 @@ def generate_uml_diagrams(package_path, output_directory, project_name='MyProjec
     quoted_package_path = f'"{package_path}"'
 
     # Build the pyreverse command
-    pyreverse_cmd = f'pyreverse -o dot -p {project_name} {quoted_package_path} -a 2 -s 2'
+    pyreverse_cmd = f'pyreverse -o dot -p {project_name} {quoted_package_path}'
 
     # Execute the pyreverse command in the package directory
     subprocess.run(pyreverse_cmd, shell=True, cwd=package_path)
@@ -25,14 +21,24 @@ def generate_uml_diagrams(package_path, output_directory, project_name='MyProjec
     for dot_file in dot_files:
         shutil.move(os.path.join(package_path, dot_file), os.path.join(output_directory, dot_file))
 
+    print("Dot files generated. Please manually edit the dot files in the output directory.")
+
+def convert_dot_to_png(output_directory):
+    # Find all dot files in the output directory
+    dot_files = [file for file in os.listdir(output_directory) if file.endswith('.dot')]
+
     # Convert 'dot' files to 'png' using the Graphviz 'dot' layout engine
     for dot_file in dot_files:
         full_dot_path = os.path.join(output_directory, dot_file)
         output_png = full_dot_path.replace('.dot', '.png')
         subprocess.run(['dot', '-Tpng', full_dot_path, '-o', output_png], shell=True)
 
+    print("PNG files have been created from the edited dot files.")
 
 # Example usage
 path_to_package = r'C:\Users\laure\Dropbox\School\BSE\Coursework\23 Fall\JuniorClinic\risk assessment\NJSESP_Data_Analysis\NJSESP_Data_Analysis_Program\NJ_SESP_Data_Analysis_Program'
 path_to_output = r'C:\Users\laure\Dropbox\School\BSE\Coursework\23 Fall\JuniorClinic\risk assessment\NJSESP_Data_Analysis\NJSESP_Data_Analysis_Program\NJ_SESP_Data_Analysis_Program\documentation'
+
 generate_uml_diagrams(path_to_package, path_to_output)
+# After manually editing the dot files, call convert_dot_to_png
+convert_dot_to_png(path_to_output)
